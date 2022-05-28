86c03ccf2b9635892250679d0c2f4f74e7114c39
Rafael Angonese <rafael.angonese09@gmail.com>
2022-05-28 03:22:42 -0300

    :bento: feat: add icons

---
+++ components/icons/profile.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    class="w-6 h-6"
+    viewBox="0 0 24 24"
+  >
+    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+    <circle cx="12" cy="7" r="4"></circle>
+  </svg>
+</template>
